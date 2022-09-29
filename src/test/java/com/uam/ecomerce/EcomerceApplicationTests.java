db465170fe04288ea577d95cdcaaaa987eb0af00
normancash <normancashnic@gmail.com>
2022-09-29 14:38:38 -0600

    ecommerce initial

---
+++ src/test/java/com/uam/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.uam.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
