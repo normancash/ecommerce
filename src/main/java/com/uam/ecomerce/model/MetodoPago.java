070981b93e39f4e4565159f90bfb92d8f12424a9
Carlos Ruiz <carlosbustillo99@gmail.com>
2022-10-26 11:04:27 -0600

    [FIX] something that i dont remember

--- src/main/java/com/uam/ecomerce/model/MetodoPago.java
+++ src/main/java/com/uam/ecomerce/model/MetodoPago.java
@@ -2,10 +2,7 @@ package com.uam.ecomerce.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -14,7 +11,11 @@ import java.util.UUID;
 public class MetodoPago {
 
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "detalle_seq",
+            sequenceName = "detalle_seq",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "detalle_seq")
     private UUID id_metodo_pago;
 
     private String credito;
--- src/main/java/com/uam/ecomerce/model/Rol.java
+++ src/main/java/com/uam/ecomerce/model/Rol.java
@@ -2,10 +2,7 @@ package com.uam.ecomerce.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -14,7 +11,11 @@ import java.util.UUID;
 public class Rol {
 
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "detalle_seq",
+            sequenceName = "detalle_seq",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "detalle_seq")
     private UUID id_rol;
 
     private String name;
