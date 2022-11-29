d3865308ac6d14258a6693b5efe060e352c910a3
normancash <normancashnic@gmail.com>
2022-11-28 22:44:18 -0600

    Swagger

--- src/main/java/com/uam/ecomerce/EcomerceApplication.java
+++ src/main/java/com/uam/ecomerce/EcomerceApplication.java
@@ -2,6 +2,12 @@ package com.uam.ecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class EcomerceApplication {
@@ -9,5 +15,6 @@ public class EcomerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcomerceApplication.class, args);
 	}
+
 
 }
